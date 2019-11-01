045ce742f8d1b71bd57d2f916a183a98a6479368
Jared A Sutton <jaredasutton@Jareds-MacBook-Pro.local>
2019-11-01 12:23:18 -0400

    Add example IO to function signature comment for mapRecurStrToObj.

--- mapRecurStrToObj.js
+++ mapRecurStrToObj.js
@@ -1,6 +1,6 @@
 /**
  * Take a string that specifies the recurrence behavior for a
- * Google Calendar recurring event and converts it to an object
+ * Google Calendar recurring event and convert it to an object
  * with the various recurrence rules delineated as key-value pairs.
  * @param {string} recurrence The recurrence string,
  *   e.g. "RRULE:FREQ=WEEKLY;WKST=MO;UNTIL=20191209T170000Z;BYDAY=SA,TH,TU"
